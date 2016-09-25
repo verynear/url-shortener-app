b2e39dcbb32d037140319718e1e2c31a1ee0914e
verynear <scott.a.near@gmail.com>
2016-09-25 07:56:30 -0500

    added first routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+
+  devise_scope :user do
+   authenticated :user do
+     root 'links#index', as: :authenticated_root
+   end
+
+   unauthenticated do
+     root 'devise/registrations#new', as: :unauthenticated_root
+   end
+ end
+
+resources :links
+
+resources :visits
+
+get '/:slug' => 'visits#create'
+
 end
